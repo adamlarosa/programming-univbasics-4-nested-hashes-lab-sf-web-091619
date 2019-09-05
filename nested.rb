46c7aea20a91b10d78cacb90c0e45ce32e31b865
Adam La Rosa <adam.larosa@gmail.com>
2019-09-05 23:03:33 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -124,6 +124,10 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
+  
+  #this is the problem line
   programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+  
+  
   return programmer_hash[:dennis_ritchie][:languages]
 end
